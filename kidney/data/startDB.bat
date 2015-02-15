92be60874edfbee1777510ef800dd744197e8c6c
RafalJagielski <rjagielski@wp.pl>
2015-02-16 00:47:51 +0100

    removed

--- kidney/data/startDB.bat
+++
@@ -1,8 +0,0 @@
-REM dataExtractor.bat
-
-SET outputDB=D:\PROGRAMOWANIE\PROJECTS\kidney\data\db\
-
-SET mongoDBLocalization="D:\wrk\mongodb\"
-
-ECHO Starting MongoDB database...
-CALL mongod -dbpath %outputDB%
